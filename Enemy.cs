bd0adcad68a26b3ca0bf887fe14d1146bb8a5bfc
tapochkek <131532931+tapochkek@users.noreply.github.com>
2023-06-07 18:59:12 +0600

    Add files via upload

---
+++ Enemy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed;
+    public int health;
+    public Transform target;
+    public int playerDamage = 2;
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+        transform.LookAt(target.position);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Player player = other.GetComponent<Player>();
+            player.TakeDamage(playerDamage);
+        }
+    }
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
