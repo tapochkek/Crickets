ec71e36d2350c6fddc66eeda24396b99a31a2054
tapochkek <131532931+tapochkek@users.noreply.github.com>
2023-06-07 18:59:39 +0600

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    public float speed;
+    public int damage;
+    private void Update()
+    {
+        transform.position += transform.forward * (speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            other.gameObject.GetComponent<Enemy>().TakeDamage(damage);
+        }
+        Destroy(gameObject);
+    }
+}
